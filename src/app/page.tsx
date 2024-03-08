13625e0dc8a4eb33196ba78d3c5952b552972b25
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2024-03-08 22:01:09 +0100

    formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ const Homepage = async () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <h1 className="text-white">
-        Izeat - share good food with good people
-      </h1>
+      <h1 className="text-white">Izeat - share good food with good people</h1>
       <div className="flex w-full flex-1 flex-col items-center gap-4">
         {!user && (
           <Link href="/login" className="text-lg text-white">
