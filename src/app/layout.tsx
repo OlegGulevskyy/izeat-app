5152853b1bf769d5615370d4e3f69d1294d94a51
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2024-03-09 18:18:50 +0100

    remove tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,13 @@
-import { TailwindIndicator } from "~/components/TailwindIndicator";
 import { Providers } from "~/providers";
-import "~/styles/globals.css";
-import { cn } from "~/utils/cn";
-import { Roboto } from "next/font/google";
-import { Toaster } from "~/components/ui/toaster";
-import { getServerUser } from "~/utils/auth";
 import { AuthProvider } from "~/providers/AuthProvider/AuthProvider";
 import { TRPCReactProvider } from "~/trpc/react";
 import { headers } from "next/headers";
+import { getServerUser } from "~/utils/auth";
 
 export const metadata = {
   title: "Izeat",
   description: "Share good food with good people",
 };
-
-const font = Roboto({
-  weight: ["100", "300", "400", "500", "700", "900"],
-  subsets: ["latin"],
-});
 
 async function RootLayout({ children }: { children: React.ReactNode }) {
   const user = await getServerUser();
@@ -27,21 +17,12 @@ async function RootLayout({ children }: { children: React.ReactNode }) {
       <html lang="en">
         <head />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            font.className,
-          )}
-        >
+        <body>
           <TRPCReactProvider headers={headers()}>
             <AuthProvider {...user}>
-              <Providers>
-                {children}
-                <Toaster />
-              </Providers>
+              <Providers>{children}</Providers>
             </AuthProvider>
           </TRPCReactProvider>
-          <TailwindIndicator />
         </body>
       </html>
     </>
