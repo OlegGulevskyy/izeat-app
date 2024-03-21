03c76c82771373fbeabee058c51fd513abf9aa9a
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2024-03-21 20:28:10 +0100

    fix some urls and page titles

--- src/components/header-nav/desktop/index.tsx
+++ src/components/header-nav/desktop/index.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 import { appNav } from "~/constants/navigation";
 import { useAppParams } from "~/hooks/use-app-params";
 import { cn } from "~/utils/cn";
 import { useTranslation } from "~/app/i18n/client";
 import { getLanguage } from "~/app/i18n/utils/get-language";
+import { useCurrentPath } from "~/hooks/use-current-path";
 
 export const HeaderDesktop = () => {
-  const pathName = usePathname();
+  const { pathWithoutLang } = useCurrentPath();
   const params = useAppParams();
   const { t } = useTranslation(getLanguage(params.lang), "common");
 
@@ -19,12 +19,12 @@ export const HeaderDesktop = () => {
           key={item.name}
           href={item.href}
           className={cn(
-            item.href === pathName
+            item.href === pathWithoutLang
               ? "border-indigo-500 text-gray-900"
               : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
             "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium",
           )}
-          aria-current={item.href === pathName ? "page" : undefined}
+          aria-current={item.href === pathWithoutLang ? "page" : undefined}
         >
           {t(item.name)}
         </Link>
